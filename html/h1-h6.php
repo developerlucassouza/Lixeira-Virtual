340a4f9d867d36c02fee37f2df8c7a875501fb27
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2024-11-21 17:17:32 -0300

    Create h1-h6.php

---
+++ html/h1-h6.php
@@ -0,0 +1,49 @@
+<?php
+include_once '../header.php';
+?>
+
+<div class="lesson">
+    <h1>Tags &lth1> - &lth6> HTML</h1>
+
+    <p>As tags <code>&lth1></code> a <code>&lth6></code> em HTML são usadas para criar títulos e subtítulos em uma página web. Elas ajudam a organizar o conteúdo, definindo uma hierarquia lógica e estrutural para textos, além de serem importantes para acessibilidade e SEO (otimização para motores de busca).</p>
+
+    <div class="code">
+        <pre>
+&lth1>Título de Hierarquia 1&lt/h1>
+&lth2>Título de Hierarquia 2&lt/h2>
+&lth3>Título de Hierarquia 3&lt/h3>
+&lth4>Título de Hierarquia 4&lt/h4>
+&lth5>Título de Hierarquia 5&lt/h5>
+&lth6>Título de Hierarquia 6&lt/h6></pre>
+    </div>
+
+    <h1 class="text-left ml-20">Título de Hierarquia 1</h1>
+    <h2 class="ml-20">Título de Hierarquia 2</h2>
+    <h3 class="ml-20">Título de Hierarquia 3</h3>
+    <h4 class="ml-20">Título de Hierarquia 4</h4>
+    <h5 class="ml-20">Título de Hierarquia 5</h5>
+    <h6 class="ml-20">Título de Hierarquia 6</h6>
+
+    <hr>
+
+    <h2>Estrutura e Hierarquia</h2>
+<ul>
+    <li>
+        <code>&lth1></code>: É o título principal, o mais importante da página.
+    </li>
+    <li>
+        <code>&lth2></code>: Subtítulo ou seção do conteúdo relacionado ao título principal.
+    </li>
+    <li>
+        <code>&lth3></code>: Subseção dentro de uma seção <code>&lth2></code>.
+    </li>
+    <li>
+        <code>&lth4></code> a <code>&lth6></code>: Subníveis de subseções, usados para conteúdos cada vez mais específicos.
+    </li>
+</ul>
+
+</div>
+
+<?php
+include_once '../footer.php';
+?>
