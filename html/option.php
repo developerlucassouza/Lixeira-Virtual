7cab64f42fe05028f3682770afcfc6cc6858ef09
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2024-12-02 18:34:06 -0300

    Create option.php

---
+++ html/option.php
@@ -0,0 +1,21 @@
+<?php
+include_once '../header.php';
+?>
+
+<div class="lesson">
+    <h1>Tag &ltoption> HTML</h1>
+    <p>A tag <code>&ltoption></code> é utilizada para definir opções dentro de uma lista suspensa criada com a tag <code>&ltselect></code>. Cada item de <code>&ltoption></code> representa uma escolha que o usuário pode selecionar. Essas opções podem ser simples texto ou valores mais complexos, dependendo do contexto.</p>
+
+    <hr>
+
+    <h2>Características Principais da Tag &ltoption></h2>
+<ul>
+    <li><strong>Opções de Seleção</strong>: Cada <code>&ltoption></code> representa uma opção individual dentro de um menu suspenso.</li>
+    <li><strong>Interatividade</strong>: Permite que o usuário selecione uma ou mais opções (dependendo do tipo de controle, como em múltiplas seleções).</li>
+    <li><strong>Valores e Texto</strong>: O valor atribuído ao <code>&ltoption></code> é o dado enviado ao servidor quando o formulário é enviado, enquanto o texto dentro da tag é o que é exibido ao usuário.</li>
+</ul>
+</div>
+
+<?php
+include_once '../footer.php';
+?>
