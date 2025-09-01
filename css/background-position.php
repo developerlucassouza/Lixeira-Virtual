1a06d7a924a1d3f9371cef4106b0cf956894d459
Lucas Souza <107756390+developerlucassouza@users.noreply.github.com>
2025-08-31 21:22:01 -0300

    Create background-position.php

---
+++ css/background-position.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../header.php';
+?>
+
+<div class="lesson">
+    <h1>background-position CSS</h1>
+
+    <p>A propriedade <code>background-position</code> define a posição inicial da imagem de fundo dentro de um elemento.</p>
+    <p>Ela é usada em conjunto com <code>background-image</code> para determinar onde a imagem deve ficar presa (exemplo: canto superior esquerdo, centro, canto inferior direito, etc.).</p>
+</div>
+
+<?php
+include_once '../footer.php';
+?>
